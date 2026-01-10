f800381282bd9653dccd82b89cc0ad0b88006b3c
nepallium <122187419+nepallium@users.noreply.github.com>
2026-01-10 09:15:52 -0500

    add: boilerplate log in page

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,3 +1,9 @@
 export default function Signup() {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <form className="max-w-md m-auto pt-24">
+        <h2 className="font-bold pb-2">Sign up</h2>
+      </form>
+    </div>
+  );
 }
